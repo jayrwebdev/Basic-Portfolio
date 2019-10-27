ae406dae4b33686ea8b4866fbce6c6d5921b4786
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-10-26 22:00:20 -0400

    Updates

---
+++ precache-manifest.2a5bccc2d64cdb952e179705c3c2e27c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b5a4d872fa6e8336545ad2b80dea32",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b1b24c02f967793ba165",
+    "url": "/my-app/static/css/main.e9016f5a.chunk.css"
+  },
+  {
+    "revision": "90fc9fbe77f011a5838f",
+    "url": "/my-app/static/js/2.a5b81c74.chunk.js"
+  },
+  {
+    "revision": "b1b24c02f967793ba165",
+    "url": "/my-app/static/js/main.7618805d.chunk.js"
+  },
+  {
+    "revision": "f8c5f9151a457093d9bb",
+    "url": "/my-app/static/js/runtime-main.5ca37c21.js"
+  }
+]);
